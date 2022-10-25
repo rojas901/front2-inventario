5bb225dcbcbd1ab594f31c83898e8c32cbbda092
Carlos Rojas <caranroja@outlook.com>
2022-10-25 12:15:48 -0500

    Se crea el grid card para mostrar la informacion de los inventarios

--- src/components/inventarios/Inventarios.jsx
+++ src/components/inventarios/Inventarios.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import inventarioServices from '../../services/inventarioService';
 
 const Inventarios = () => {
@@ -7,7 +7,7 @@ const Inventarios = () => {
 
   const listarInventarios = async () => {
     try {
-      const {data} = await inventarioServices.get();
+      const { data } = await inventarioServices.get();
       setInventarios(data)
     } catch (error) {
       console.log(error)
@@ -16,12 +16,64 @@ const Inventarios = () => {
 
   useEffect(() => {
     listarInventarios();
-  }, []); 
+  }, []);
 
   return (
-    <>
-      <h1>Inventarios</h1>
-    </>
+    <div className='container-fluid'>
+      <h1 className='text-center'>Inventarios</h1>
+      <hr />
+      <div className="row row-cols-1 row-cols-md-3 g-4 mb-4">
+        {
+          inventarios.map((inventario) => {
+            return (
+              <div className="col">
+                <div className="card">
+                  <img src={inventario.fotoEquipo} className="card-img-top" alt={inventario.tipo.nombre} height="300rem"/>
+                  <div className="card-body">
+                    <h5 className="card-title text-center">Caracteristicas</h5>
+                    <hr />
+                    <p className="card-text">
+                      <ul className='list-unstyled'>
+                        <li>
+                          <strong>Serial: </strong> {inventario.serial}
+                        </li>
+                        <li>
+                          <strong>Modelo: </strong>{inventario.modelo}
+                        </li>
+                        <li>
+                          <strong>Descripcion: </strong>{inventario.descripcion}
+                        </li>
+                        <li>
+                          <strong>Color: </strong>{inventario.color}
+                        </li>
+                        <li>
+                          <strong>Fecha de compra: </strong>{inventario.fechaCompra}
+                        </li>
+                        <li>
+                          <strong>Precio: </strong>{inventario.precio}
+                        </li>
+                        <li>
+                          <strong>Usuario: </strong>{inventario.usuario.nombre}
+                        </li>
+                        <li>
+                          <strong>Marca: </strong>{inventario.marca.nombre}
+                        </li>
+                        <li>
+                          <strong>Estado: </strong>{inventario.estado.nombre}
+                        </li>
+                        <li>
+                          <strong>Tipo: </strong>{inventario.tipo.nombre}
+                        </li>
+                      </ul>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        }
+      </div>
+    </div>
   );
 }
 
